6ed3220e6ef3637df8fdaa355cebf50b7bfbb5b3
Roderick Buo <roderickbuo31@gmail.com>
2021-03-26 12:25:18 -0700

    Added New Indicators

---
+++ iexdiscordbot/cogs/commoditychannelindex.py
@@ -0,0 +1,68 @@
+import discord
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+import requests
+import asyncio
+import json
+import numpy as np
+import pandas as pd
+from datetime import timedelta, date, datetime
+import tracemalloc
+
+#import iexdiscordbot.helper.closingpricehelper as cphelp
+
+from iexdiscordbot.helper.closingpricehelper import closingPrices
+
+"""IEX Variables[Sandbox/Stable]"""
+
+load_dotenv()
+IEX_API_KEY = os.getenv('IEX_API_KEY')
+base_url = 'https://sandbox.iexapis.com/'
+version = 'stable/'
+
+"""Asyncio Declaration"""
+loop = asyncio.get_event_loop()
+
+"""This is the long way of writing commands"""
+class cci(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('CCI Command Loaded')
+
+#Calculate RSI Here
+    @commands.command()
+    async def cci(self, ctx, *, message):
+
+        #CCI = ([(highest + lowest + close)/3] - n Moving Average of M) / (0.015 * mean deviation between the mean price and moving average of mean prices)
+        api_call = f'{base_url+version}stock/{message}/chart/3m?token={IEX_API_KEY}'
+        requestHistoricalPrices = requests.get(api_call)
+        dataHistoricalPrices = requestHistoricalPrices.json()
+        data = pd.DataFrame(dataHistoricalPrices)
+
+        aggregatedData = data[['high','low', 'close', 'date']]
+        print(f'{data}')
+
+        date = []
+        high = []
+        low = []
+        closingPrice = []
+
+        for label, row in aggregatedData.iterrows():
+           date.append(row['date'])
+           high.append(row['high'])
+           low.append(row['low'])
+           closingPrice.append(row['close'])
+
+        typicalPrice = (data['high'] + data['low'] + data['close']) / 3
+        #print(f'{data['high']} + {data['low']} + {data['close']}')
+        CCI = pd.Series((typicalPrice - typicalPrice.rolling(20).mean()) / (0.015 * typicalPrice.rolling(20).std()), name = 'CCI')
+        data = data.join(CCI)
+        print(f'{typicalPrice} \n {CCI}')
+
+def setup(client):
+    client.add_cog(cci(client))
---
+++ iexdiscordbot/cogs/register.py
@@ -0,0 +1,57 @@
+# db_host="localhost"
+# db_database="iexDiscordDB"
+# db_user="postgres"
+# db_password="1234"
+import discord
+from discord.ext import commands
+import asyncpg
+from iexdiscordbot.database.config import config
+
+class register(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Register Command Loaded')
+
+    @commands.command()
+    async def register(self, ctx):
+        connection = None
+        id = ctx.message.author.id
+        name = ctx.message.author.name
+        discriminator = ctx.message.author.discriminator
+        print(f'{id} | {name} | {discriminator}')
+        try:
+            params = config()
+            print('Connecting to the postgreSQL database...')
+            connection = await asyncpg.connect(**params)
+
+            #create a cursor
+            # async with connection.transaction():
+            #     crsr = await connection.cursor('INSERT INTO users (id, name);')
+
+            await connection.execute('''
+                INSERT INTO users (id, name, discriminator) VALUES($1, $2, $3)
+            ''', id, name, discriminator)
+
+                #await crsr.execute('SELECT version()') #use this coroutine to execute an SQL command
+
+            #run the query SQL input here, have atleast the watchlist command output here
+                # db_version = await crsr.fetchrow()
+                # print(f'PostgreSQL database version: {db_version}')
+
+
+                #print(db_version)
+                #await crsr.close() #We do not need to use close because it is done automatically
+        # except(Exception, asyncpg.DatabaseError) as error:
+        #     print(f'Exception: {error}')
+        #     print(error)
+        finally:
+            if connection is not None:
+                await connection.close()
+                print('Database connection terminated.')
+
+def setup(client):
+    client.add_cog(register(client))
